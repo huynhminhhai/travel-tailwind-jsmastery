725d1bd9164e24abed4fcbf8faac6bed38f86a46
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-15 16:15:03 +0700

    [UPDATE] build footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
   return (
     <footer className='flexCenter mb-24'>
       <div className='padding-container max-container flex flex-col w-full gap-14'>
-        <div className='flex flex-col items-start justify-center gap-[100px] md:flex-row'>
+        <div className='flex flex-col items-start justify-center md:flex-row'>
           <Link href={'/'} className='mb-10'>
             <Image src='/hilink-logo.svg' alt='hilink logo' width={74} height={29} />
           </Link>
